e1c1741be826197c42198adf069e8a62c18b9de8
Miguelangel Weill <miguelangel.weill@gmail.com>
2020-08-26 18:18:20 -0400

    finished the homework

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -1,16 +1,98 @@
-// array of questions for user
-const questions = [
+const fs=require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+const generateMarkdown = require("./utils/generateMarkdown");
+const Choice = require("inquirer/lib/objects/choice");
+const Choices = require("inquirer/lib/objects/choices");
+const { resolve } = require("path");
+const { rejects } = require("assert");
 
-];
+// array of questions for user
+ const questions=([
+   {
+     type: "input",
+     name: "title",
+     message: "What is your project title?"
+   }, 
+   {
+     type: "checkbox",
+     name: "license",
+     message: "Choose a lisence.",
+     choices:[
+       "MIT",
+       "Apache",
+       "GPL",
+       "Public"
+     ]
+   },
+   {
+     type: "input",
+     name: "description",
+     message: "Please provide your project's description"
+   },
+   {
+     type:"input",
+     name:"projectImage",
+     message:"Provide a screen shot of your project"
+   },
+   {
+     type: "input",
+     name: "installation",
+     message: "Please provide the installation instructions"
+   },
+   {
+     type: "input",
+     name: "usage",
+     message: "Please provide the project usage"
+   },
+   {
+     type: "input",
+     name: "contributing",
+     message: "Please provide the contributing parties"
+   },
+   {
+     type: "input",
+     name: "test",
+     message: "Please provide the project tests"
+   },
+   {
+     type: "input",
+     name: "username",
+     message: "What is your github user name?"
+   },
+   {
+     type: "input",
+     name: "repo",
+     message: "What is your Git hub repository link?"
+   },
+   {
+     type:"input",
+     name:"image",
+     message:"If you wish to add an image please enter the file path to that image or URL of the image"
+   }
+ ]);
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+inquirer
+.prompt(questions)
+.then(function(data){
+  const queryUrl = `https://api.github.com/users/${data.username}`
 
-// function to initialize program
-function init() {
+  axios.get(queryUrl).then(res=>{
 
-}
+    const githubInfos={
+      imageGitHub:res.data.avatar_url,
+      profile:res.data.url
+    }
+    fs.writeFile("README.md",generateMarkdown(data,githubInfos),err=>{
+    if(err){
+      throw err
+    }else{
+      console.log("Success file was created!!!")
+    }
+  })
+  })
+  
+})
 
-// function call to initialize program
-init();
+
--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -1,6 +1,50 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
+function generateMarkdown(data,githubInfos) {
   return `# ${data.title}
+  ## license
+  ${data.license
+    .map(item => {
+      return `![${item}](https://img.shields.io/badge/license-${item}-blue)`;
+    })
+    .join("  ")}
+
+  ## Description
+  ${data.description}
+
+  ![Image Of Project](${data.projectImage})
+
+  ## Table of contents
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Licence](#License)
+- [Contributors](#Contributors)
+- [Test](#Test)
+- [Repository Link](#Repository)
+- [GitHub Info](#GitHub)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Constributors
+  ${data.contributing}
+
+  ## Test
+  ${data.test}
+
+  ## Git hub user name
+  ${data.username}
+
+  ## Repository
+
+![Image of generator](${githubInfos.imageGitHub})
+
+[Git hub profile](${githubInfos.profile})
+  
+[This is my Git Hub repository](${data.repo})
 
 `;
 }
